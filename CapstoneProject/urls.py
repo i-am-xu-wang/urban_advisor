25f9b47d2b7c6feede5b3b178584bc2bcdd3eda2
ou <xu16@vt.edu>
2021-10-22 22:53:14 +0300

    set up url address for index and report page

--- CapstoneProject/urls.py
+++ CapstoneProject/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from capstone import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index_page, name="index"),
+    path('report/', views.report_page, name="report"),
 ]
--- capstone/views.py
+++ capstone/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index_page(request):
+    return render(request,
+                  "capstone/questionnaire.html",
+                  )
+
+
+def report_page(request):
+    return render(request,
+                  "capstone/report.html",
+                  )
