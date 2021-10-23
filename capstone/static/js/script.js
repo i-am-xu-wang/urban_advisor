63f635f2275ea593035fd002882180e6cc031ad7
ou <xu16@vt.edu>
2021-10-24 00:57:21 +0300

    recopy the fail/always code from Ajax webiste

--- capstone/static/js/script.js
+++ capstone/static/js/script.js
@@ -3,7 +3,7 @@ const csrftoken = getCookie('csrftoken');
 $(document).ready(function () {
     //submit questionnaire when clicking submit button
     var household_members = $('#household-options').find(":selected").text().toString();
-   // var obj = $('#quiz').serializeJSON();
+    // var obj = $('#quiz').serializeJSON();
     console.log(household_members)
     $('#submit-button').click(function () {
         console.log("Submit button clicked!");
@@ -12,25 +12,25 @@ $(document).ready(function () {
             // The URL for the request
             url: "/",
             // The data to send (will be converted to a query string)
-           data: {
-                member:household_members
-           },
+            data: {
+                member: household_members
+            },
             type: "POST",
             // The type of data we expect back
             dataType: "json",
-            headers: {'X-CSRFToken': csrftoken,'x-requested-with':XMLHttpRequest}
+            headers: {'X-CSRFToken': csrftoken, 'x-requested-with': XMLHttpRequest}
         })
             .done(function (json) {
                 alert("Receive Ajax request");
-               // console.log(JSON.stringify(obj));
+                //console.log(JSON.stringify(obj));
             })
             .fail(function (xhr, status, errorThrown) {
                 alert("Sorry, there was a problem!");
                 console.log("Error: " + errorThrown);
                 console.log("Status: " + status);
                 console.dir(xhr);
-                console.log(JSON.stringify(obj));
             })
+            // Code to run regardless of success or failure;
             .always(function (xhr, status) {
                 alert("The request is complete!");
             });
