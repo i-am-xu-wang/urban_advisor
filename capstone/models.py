be37630a25afe3bfa598425a354ba51a28861ffe
Chris Luersen <chrisluersen@gmail.com>
2021-11-09 23:05:42 -0500

    Connected db to model.py/admin.py and migrated to app

--- capstone/models.py
+++ capstone/models.py
@@ -3,3 +3,14 @@ from django.db import models
 
 # Create your models here.
 
+class Expense(models.Model):
+    expenses = models.CharField(max_length=200)
+    roanoke = models.IntegerField()
+    dc = models.IntegerField()
+    philly = models.IntegerField()
+    boston = models.IntegerField()
+    seattle = models.IntegerField()
+    sf = models.IntegerField()
+
+    def __str__(self):
+        return self.expenses
