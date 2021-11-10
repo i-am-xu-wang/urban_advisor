9e489f393b5e23d0ae8d126f15c1a5729e77f774
Chris Luersen <chrisluersen@gmail.com>
2021-11-09 23:22:16 -0500

    Update int field to float, and sql types to real

--- capstone/models.py
+++ capstone/models.py
@@ -4,13 +4,13 @@ from django.db import models
 # Create your models here.
 
 class Expense(models.Model):
-    expenses = models.CharField(max_length=200)
-    roanoke = models.IntegerField()
-    dc = models.IntegerField()
-    philly = models.IntegerField()
-    boston = models.IntegerField()
-    seattle = models.IntegerField()
-    sf = models.IntegerField()
+    expenses = models.CharField(max_length=250)
+    roanoke = models.FloatField()
+    dc = models.FloatField()
+    philly = models.FloatField()
+    boston = models.FloatField()
+    seattle = models.FloatField()
+    sf = models.FloatField()
 
     def __str__(self):
         return self.expenses
